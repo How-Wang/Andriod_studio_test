27443b59bc18edae8144b0317d0a422e3644ba64
hank95179 <hank95179@gmail.com>
2023-06-05 14:02:36 +0800

    get text

--- app/src/main/java/com/example/tutorial/login_act.java
+++ app/src/main/java/com/example/tutorial/login_act.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.util.Log;
 import android.widget.ImageView;
 import android.view.animation.Animation;
 
@@ -16,7 +17,18 @@ public class login_act extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
     public void SignupAct(View v){
+        TextView t = findViewById(R.id.login_account);
+        String loginAC = t.getText().toString();
+        Log.d("info", loginAC);
+
+        TextView x = findViewById(R.id.LoginPassword);
+        String loginPw = x.getText().toString();
+        Log.d("info", loginPw);
         Intent i = new Intent(this, Signup_Act.class);
         startActivity(i);
+    }
+
+    public void GetLoginAcc(View v){
+
     }
 }

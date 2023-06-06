1da4011893aec31b83e27082f06cbd8cf329997f
hank95179 <hank95179@gmail.com>
2023-06-06 16:11:56 +0800

    change start menu

--- app/src/main/java/com/example/tutorial/Signup_Act.java
+++ app/src/main/java/com/example/tutorial/Signup_Act.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.util.Log;
 import android.widget.ImageView;
 import android.view.animation.Animation;
@@ -16,7 +17,7 @@ public class Signup_Act extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
     }
-    public void signupClicked(){
+    public void signupClicked(View v){
         TextView t = findViewById(R.id.SignupAccount);
         String signupAC = t.getText().toString();
         Log.d("signup_info", signupAC);
@@ -24,7 +25,12 @@ public class Signup_Act extends AppCompatActivity {
         TextView x = findViewById(R.id.SignupPassword);
         String signupPw = x.getText().toString();
         Log.d("signup_info", signupPw);
-//        Intent i = new Intent(this, login_act.class);
-//        startActivity(i);
+
+//        if(帳號註冊過){//提示訊息
+//            Toast.makeText(getApplicationContext(), "帳號已註冊", Toast.LENGTH_SHORT).show();
+//        }
+        Toast.makeText(getApplicationContext(), "註冊成功", Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(this, login_act.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/tutorial/login_act.java
+++ app/src/main/java/com/example/tutorial/login_act.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.util.Log;
-import android.widget.ImageView;
+import android.widget.Toast;
 import android.view.animation.Animation;
 
 public class login_act extends AppCompatActivity {
@@ -35,10 +35,16 @@ public class login_act extends AppCompatActivity {
         TextView x = findViewById(R.id.LoginPassword);
         String loginPw = x.getText().toString();
         Log.d("login_info", loginPw);
+//        loginAC = 登入帳號
+//        loginPW = 登入密碼
 //        Intent i = new Intent(this, Signup_Act.class);
 //        startActivity(i);
-    }
-    public void GetLoginAcc(View v){
-
+//        if(帳號密碼符合){
+//            Intent i = new Intent(this, 登入完要跳轉的頁面);
+//            startActivity(i);
+//        }
+        Toast.makeText(getApplicationContext(), "登入成功", Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }

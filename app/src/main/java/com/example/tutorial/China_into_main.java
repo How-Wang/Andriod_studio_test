5ba26e7e512103cd7fe4f2fc92e3c70ae71f4bca
hank95179 <hank95179@gmail.com>
2023-06-07 21:46:48 +0800

    all intro finish

--- app/src/main/java/com/example/tutorial/China_into_main.java
+++ app/src/main/java/com/example/tutorial/China_into_main.java
@@ -1,8 +1,14 @@
 package com.example.tutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.util.Log;
+import android.widget.Toast;
+import android.view.animation.Animation;
 
 public class China_into_main extends AppCompatActivity {
 
@@ -10,5 +16,21 @@ public class China_into_main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_china_into_main);
+    }
+    public void btn1(View v){
+        Intent i = new Intent(this, China_intro1.class);
+        startActivity(i);
+    }
+    public void btn2(View v){
+        Intent i = new Intent(this, China_intro2.class);
+        startActivity(i);
+    }
+    public void btn3(View v){
+        Intent i = new Intent(this, China_intro3.class);
+        startActivity(i);
+    }
+    public void btn4(View v){
+        Intent i = new Intent(this, China_intro4.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/tutorial/China_intro1.java
@@ -0,0 +1,14 @@
+package com.example.tutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class China_intro1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_china_intro1);
+    }
+}
